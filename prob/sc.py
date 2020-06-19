2f82154f0900d808b82b8e445115336b05f02786
Bhumbra <bhumbra@gmail.com>
2020-06-19 16:49:41 +0100

    Apply code corrections

--- prob/sc.py
+++ prob/sc.py
@@ -11,7 +11,9 @@ class SC:
 
   # Protected
   _rvs = None
+  _nrvs = None
   _index = None
+
 
 #-------------------------------------------------------------------------------
   def __init__(self, *args):
@@ -19,30 +21,30 @@ class SC:
 
 #-------------------------------------------------------------------------------
   def set_rvs(self, *args):
-    if len(args) == 1:
-      if isinstance(args[0], (tuple, list)):
-        args = args[0]
-      elif isinstance(args[0], RC):
-        args = RC.ret_rvs()
-      else:
-        raise TypeError("Unknown RVs argument type: {}".format(type(args[0])))
-    if isinstance(args, dict):
-      for rv in args.values():
-        self.add_rv(rv)
-    else:
-      for rv in args:
-        self.add_rv(rv)
+    if len(args) == 1 and isinstance(args[0], (SC, dict, set, tuple, list)):
+      args = args[0]
+    self.add_rv(args)
     return self.ret_rvs()
 
 #-------------------------------------------------------------------------------
   def add_rv(self, rv):
     if self._rvs is None:
       self._rvs = collections.OrderedDict()
-    assert isinstance(rv, RV), \
-        "RV not a RandVar instance but of type: {}".format(type(rv))
-    assert rv.name not in self._rvs.keys(), \
-        "Existing RV name {} already present in collection".format(rv.name)
-    self._rvs.update({rv.name: rv})
+    if isinstance(rv, (SC, dict, set, tuple, list)):
+      rvs = rv
+      if isinstance(rvs, SC):
+        rvs = rvs.ret_rvs()
+      if isinstance(rvs, dict):
+        rvs = rvs.values()
+      [self.add_rv(rv) for rv in rvs]
+    else:
+      assert isinstance(rv, RV), \
+          "Input not a RV instance but of type: {}".format(type(rv))
+      assert rv.name not in self._rvs.keys(), \
+          "Existing RV name {} already present in collection".format(rv.name)
+      self._rvs.update({rv.name: rv})
+    self._nrvs = len(self._rvs)
+    return self._nrvs
 
 #-------------------------------------------------------------------------------
   def __getitem__(self, key):
