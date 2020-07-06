4acfc9028e24832227fd6b3025867fcdd3087aa8
Bhumbra <bhumbra@gmail.com>
2020-07-06 14:43:47 +0100

    Support sampling through pfun and vfun

--- main/zipf1dsamp.py
+++ main/zipf1dsamp.py
@@ -1,12 +1,13 @@
-# Example of sampling from a normal probability density function
-import scipy.stats
+# Example of sampling from a Zipfian probability density function
+import numpy as np
 import matplotlib
 matplotlib.use("Qt5Agg")
 from pylab import *; ion()
 import prob
 
-norm_range = {-2., 2.}
+zipf_range = {0.1, 10.}
 set_size = {-10000} # size negation denotes random sampling
-rv = prob.RV("trun_norm", norm_range, prob=scipy.stats.norm,loc=0, scale=1)
+rv = prob.RV("zipf_rv", zipf_range)
+rv.set_vfun((np.log, np.exp))
 samples = rv.eval_vals(set_size)
 hist(samples, 100)
