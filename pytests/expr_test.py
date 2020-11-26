6d7012fe21319ed7bc7483412456569d6f6b076f
Bhumbra <bhumbra@gmail.com>
2020-11-26 14:01:06 +0000

    Attempt to fix pytesting

--- pytests/expr_test.py
+++ pytests/expr_test.py
@@ -3,7 +3,7 @@
 #-------------------------------------------------------------------------------
 import numpy as np
 import scipy.stats
-from probayes import Expression
+import probayes as pb
 import pytest
 
 #-------------------------------------------------------------------------------
@@ -16,7 +16,7 @@ SCIPY_TESTS = [
 @pytest.mark.parametrize("dist, values, kwds", SCIPY_TESTS)
 def test_scipy(dist, values, kwds):
   print("Testing distribution {}".format(dist))
-  expr = Expression(dist, **kwds)
+  expr = pb.Expression(dist, **kwds)
   prob = expr['prob'](values)
   logp = expr['logp'](values)
   assert np.allclose(prob, np.exp(logp)), \
