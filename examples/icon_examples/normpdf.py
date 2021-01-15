5cb81d0ff6f84776fb4e155f7b29c556eea79d4c
Bhumbra <bhumbra@gmail.com>
2021-01-15 16:40:05 +0000

    Prepare icon test directory

--- examples/icon_examples/normpdf.py
+++ examples/icon_examples/normpdf.py
@@ -1,18 +1,13 @@
-# Example of a 1D normal probability density function
+# TODO - work in progress
+""" Example of a 1D normal probability density function
+"""
 import sympy.stats
 import matplotlib
 matplotlib.use("Qt5Agg")
 from pylab import *; ion()
 import probayes as pb
 
-class fingy:
-  def __getitem__(*args, **kwds):
-    _args, _kwds = args, kwds
-  def __call__(*args, **kwds):
-    _args, _kwds = args, kwds
-
 set_lims = [-3., 3.]
 set_size = {100}
 rv = pb.RV("norm", float)
 rv.set_prob(sympy.stats.Normal)
-a = fingy()
